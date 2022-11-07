4615340c234324cc86abcf4157f1e7424204311f
sumanth-healium <sumanth@healium.ai>
2022-11-07 09:25:04 +0530

    AUCTION details

--- src/shared/components/details/details.tsx
+++ src/shared/components/details/details.tsx
@@ -41,16 +41,121 @@ function Details() {
 }
   return (
     <>
-      {Object.values(data).length != 0 && (
+
+
+    <section className="text-white body-font overflow-hidden">
+        <div className="container px-5 mx-auto">
+          <div className="flex flex-wrap mx-5">
+            <div className="p-12 md:w-1/2 flex flex-col items-start">
+              {Object.values(data).length != 0 && (
+                <img
+                  alt="ecommerce"
+                  className="w-full sm:h-auto md:h-[25rem] object-center  rounded"
+                  src={getInfuraURL(data?.meta?.thumbnail)}
+                />
+              )}
+
+              <div className="py-5 w-[100%]">
+                {/* <Bids w_percentage={"100"} bids={bids} /> */}
+                <div className={`overflow-hidden border shadow sm:rounded-lg md:w-[100%] sm:w-auto`}>
+              <div className="px-4 py-5 sm:px-6">
+                <h3 className="text-lg font-medium leading-6">{location.pathname.includes("auction")?"Bids":"Offers"}</h3>
+              </div>
+              <div className="border-t border-gray-200">
+                <dl>
+                  <div className="px-4 py-5 grid grid-cols-3 gap-4 px-6 text-center">
+                    <dt className="text-xs text-gray-500">Price</dt>
+                    <dt className="text-xs text-gray-500">USD Price</dt>
+                    <dt className="text-xs text-gray-500">Price</dt>
+                  </div>
+                  {bids?.map((val: any, index: any) => (
+                    <div
+                      className={` ${
+                        index != 0 && "border-t"
+                      } px-4 py-4  grid grid-cols-3 gap-4 px-6 text-center`}
+                    >
+                      <dt className="text-sm font-medium text-white">
+                        {val.price}
+                      </dt>
+                      <dt className="text-sm font-medium text-gray-500">
+                        {val.usd}
+                      </dt>
+                      <dt className="text-sm font-medium text-white">
+                        {val.from}
+                      </dt>
+                    </div>
+                  ))}
+                </dl>
+              </div>
+            </div>
+              </div>
+
+            </div>
+
+            <div className="p-12 md:w-1/2 flex flex-col items-start">
+              <h2 className="font-Montserrat text-[24px] font-extrabold">
+                {data?.meta?.name}
+              </h2>
+              <div className="h-2"></div>
+              <p className="font-Montserrat text-[16px] font-extralight">
+                Author{" "}
+                <span className="font-extrabold">{data?.owner?.name}</span>
+              </p>
+              <div className="h-4"></div>
+              <p className="font-light">{data?.meta?.description}</p>
+
+              <div className="py-8 w-[100%]">
+                <div className="mx-auto">
+                  <dl className="mt-5 text-center grid grid-cols-1 rounded-2xl border overflow-hidden shadow md:grid-cols-3 items-center">
+                    <div className="md:px-4 md:py-5 sm:p-6">
+                      <dt className="font-semibold text-center text-2xl">
+                        <div>
+                          <p className="text-center text-xl whitespace-nowrap">Auction Ends In</p>
+                        </div>
+                      </dt>
+                      <dt className="text-base hover:cursor-pointer font-normal whitespace-nowrap text-gray-400">
+                      {data?.auction?.deadline}
+                      </dt>
+                    </div>
+                    <div>
+                      <p className="text-3xl">/</p>
+                    </div>
+                    <div className="md:px-4 md:py-5 sm:p-6">
+                      <dt className="font-semibold text-center text-xl">
+                        <div>
+                          <p className="text-center text-xl whitespace-nowrap">Last Bid</p>
+                        </div>
+                      </dt>
+                      <dt className="text-base font-normal text-gray-400 whitespace-nowrap">
+                      {data?.auction?.highestBid} ETH
+                      </dt>
+                    </div>
+                  </dl>
+                </div>
+              </div>
+
+              <div className="w-[100%] border dark:border-blue-500 flex items-center justify-between rounded-lg text-gray-400">
+                  <h4 className="px-5 w-[100%]">
+                    {" "}
+                    Minimum bid {data?.auction?.minBid} ETH
+                  </h4>
+                  <button className="cursor-pointer w-[50%] border-blue-600 border bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 px-5 py-5 rounded-lg text-center text-white">
+                    Place Bid
+                  </button>
+                </div>
+
+              <div className="py-16 w-[100%]">
+              <LineChart className="py-6" w_percentage={"100"} chart_data={chart_data} />
+              </div>
+            </div>
+          </div>
+         
+
+        </div>
+      </section>
+      {/* {Object.values(data).length != 0 && (
         <>
           <div className="flex lg:flex-row md:flex-row gap-20 m-3 mt-20 lg:mx-32 md:mx-32 relative sm:flex-col xs:flex-col z-[-1]">
-            {/* <div className="grid grid-flow-col m-3 mt-20 mx-32 relative z-[-1] gap-10"> */}
-            {/* <img
-              src={getInfuraURL(data?.meta?.thumbnail)}
-              alt="Front of zip tote bag with white canvas, black canvas straps and handle, and black zipper pulls."
-              className="sm:w-auto lg:w-[50%] md:w-[50%] object-center object-cover z-1000 rounded-lg"
-            /> */}
-
             <div className="sm:w-auto lg:w-[50%] md:w-[50%] z-1000 rounded-lg">
                   <img
                     src={getInfuraURL(data?.meta?.thumbnail)}
@@ -161,13 +266,12 @@ function Details() {
             </div>
           </div>
 
-          {/* <div className="py-5 container m-auto mt-20 flex flex-row gap-20"> */}
           <div className="flex lg:flex-row md:flex-row gap-20 m-3 mt-20 lg:mx-32 md:mx-32 xs:flex-col relative sm:flex-col z-[-1]">
             <Bids className="w-1/2 " bids={bids} />
             <LineChart className="w-1/2" chart_data={chart_data} />
           </div>
         </>
-      )}
+      )} */}
     </>
   );
 }
